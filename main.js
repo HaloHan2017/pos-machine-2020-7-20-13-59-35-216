bf252c1495671fe855c11493536fdf737d6aef48
HaloHan2017 <2332722908@qq.com>
2020-07-22 17:45:19 +0800

    [code review] [jerry] fix: refactor code in main.js ,

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const items = [
+const DATA_ITEMS = [
     {
         barcode: 'ITEM000000',
         name: 'Coca-Cola',
@@ -30,54 +30,73 @@ const items = [
         price: 4
     }
 ];
+
 function printReceipt(barcodes) {
-    if(isBarcodesEmpty(barcodes)){
+    if (barcodes.length == 0) {
         return "***<store earning no money>Receipt ***" +
-                "Total: 0 (yuan)\n" +
-                "**********************";
+            "Total: 0 (yuan)\n" +
+            "**********************";
     }
-    console.log(getReceiptStr(barcodes));
+    console.log(getReceiptStr(getItemQuantityMap(barcodes)));
 }
-function getReceiptStr(barcodes){
-    let itemMap = getItemMapByBarcodes(barcodes);
+
+// function getReceiptStr(barcodes) {
+//     let itemQuantityMap = getItemQuantityMap(barcodes);
+//     return getFormatReceiptDetails(iemQuantityMap);
+// }
+
+function getReceiptStr(itemQuantityMap) {
     let resultStr = "\n***<store earning no money>Receipt ***\n";
     let total = 0;
-    itemMap.forEach(function(value , key){
-        let item = getItemInfoByBarcode(key);
-        resultStr += "Name: " + item.name + ", Quantity: " + value + ", Unit price: " + item.price + " (yuan), Subtotal: " + (item.price * value) + " (yuan)\n";
-        total += item.price * value;
+    itemQuantityMap.forEach(function (itemQuantity, barcode) {
+        let item = getItemByBarcode(barcode);
+        resultStr += `Name: ${item.name}, Quantity: ${itemQuantity}, Unit price: ${item.price} (yuan), Subtotal: ${item.price * itemQuantity} (yuan)\n`;
+        total += item.price * itemQuantity;
     });
     resultStr += "----------------------\n";
-    resultStr += "Total: " + total + " (yuan)\n";
+    resultStr += `Total: ${total} (yuan)\n`;
     resultStr += "**********************";
     return resultStr;
 }
 
-function isBarcodesEmpty(barcodes){
-    return barcodes.length == 0;
-}
-
-function getItemMapByBarcodes(barcodes){
-    let itemMap = new Map();
-    // 统计遍历完之后，得到了对应 barcode的item的数量
-    for(let i = 0; i < barcodes.length; i++) {
-        let barcode = barcodes[i];
-        let item = getItemInfoByBarcode(barcode);
-        if(itemMap.has(item.barcode)){
-            itemMap.set(item.barcode, itemMap.get(barcode) + 1); // 对应barcode的item数量 +1
-        }else {
-            itemMap.set(item.barcode , 1);  // 第一次出现的item，数量设为 1
+// todo 更改方法的命名
+function getItemQuantityMap(barcodes) {
+    let itemQuantityMap = new Map();
+    // todo 使用英文注释，并且尽量少用注释
+    // todo 使用forEach去进行遍历
+    for (let barcode of barcodes) {
+        if (itemQuantityMap.has(barcode)) {
+            itemQuantityMap.set(barcode, itemQuantityMap.get(barcode) + 1);
+        } else {
+            itemQuantityMap.set(barcode, 1);
         }
     }
-    return itemMap;
+    // for(let i = 0; i < barcodes.length; i++) {
+    //     let barcode = barcodes[i];
+    //     let item = getItemInfoByBarcode(barcode);
+    //     if(itemMap.has(item.barcode)){
+    //         itemMap.set(item.barcode, itemMap.get(barcode) + 1); // 对应barcode的item数量 +1
+    //     }else {
+    //         itemMap.set(item.barcode , 1);  // 第一次出现的item，数量设为 1
+    //     }
+    // }
+    return itemQuantityMap;
 }
 
-function getItemInfoByBarcode(barcode){
-    for(let i = 0; i < items.length; i++){
-        if(barcode == items[i].barcode){
-            return items[i];
+function getItemByBarcode(barcode) {
+    // DATA_ITEMS.find(item => {
+    //     if(barcode === item.barcode){
+    //         return item;
+    //     }
+    // })
+    // return null;
+    for (let i = 0; i < DATA_ITEMS.length; i++) {
+        if (barcode == DATA_ITEMS[i].barcode) {
+            return DATA_ITEMS[i];
         }
     }
+    // todo 增加返回值
+    return null;
 }
 
 
